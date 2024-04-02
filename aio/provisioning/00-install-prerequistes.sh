370c5b56635148a839c03992a0ae7aa33b45fc4b
Emmeline Hoops <108825895+chaosdinosaur@users.noreply.github.com>
2024-04-02 16:15:32 -0500

    Update 00-install-prerequistes.sh

--- aio/provisioning/00-install-prerequistes.sh
+++ aio/provisioning/00-install-prerequistes.sh
@@ -4,36 +4,31 @@ set -e
 
 echo "Installing Prerequites..."
 
-# This env var is important to allow k3s to support shared mounts, required for CSI driver
-# Temporary fix until made default https://github.com/k3d-io/k3d/pull/1268#issuecomment-1745466499
-export K3D_FIX_MOUNTS=1
-
-# Install K3D
-if ! command -v k3d >/dev/null 2>&1; then
-    echo "K3D not found, installing"
-    curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | sh
+# Install K3S
+if ! command -v k3s >/dev/null 2>&1; then
+    echo "K3S not found, installing"
+    curl -sfL https://get.k3s.io | sh -
+    curl -sfL https://get.k3s.io | K3S_URL=https://myserver:6443 K3S_TOKEN=mynodetoken sh -
 else
-    echo "K3D already installed"
-fi
-# Create local registry for K3D and local development
-if [ "$(docker ps -f name=k3d-devregistry.localhost -q)" ]; then
-    echo "Registry already exists so this is a rebuild of Dev Container, skipping"
-else
-    k3d registry create devregistry.localhost --port 5500
+    echo "K3S already installed"
 fi
 
-# Create k3d cluster with NFS support and forwarded ports
-# See https://github.com/jlian/k3d-nfs
-if [ "$(k3d cluster list | grep devcluster)" ]; then
-    echo "Cluster already exists so this is a rebuild of Dev Container, resetting context"
-    k3d kubeconfig merge devcluster --kubeconfig-merge-default
-else
-    k3d cluster create devcluster --registry-use k3d-devregistry.localhost:5500 -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1 \
-    -p '1883:1883@loadbalancer' \
-    -p '8883:8883@loadbalancer' \
-    -p '6001:6001@loadbalancer' \
-    -p '4000:80@loadbalancer'
-fi
+mkdir ~/.kube
+sudo KUBECONFIG=~/.kube/config:/etc/rancher/k3s/k3s.yaml kubectl config view --flatten > ~/.kube/merged
+mv ~/.kube/merged ~/.kube/config
+chmod  0600 ~/.kube/config
+export KUBECONFIG=~/.kube/config
+#switch to k3s context
+kubectl config use-context default
+
+echo fs.inotify.max_user_instances=8192 | sudo tee -a /etc/sysctl.conf
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
+
+sudo sysctl -p
+
+echo fs.file-max = 100000 | sudo tee -a /etc/sysctl.conf
+
+sudo sysctl -p
 
 # Install Azure CLI
 if ! command -v az >/dev/null 2>&1; then
