1ec1a8b0587039af069998f6cd3c7d8998448a55
Emmeline Hoops <108825895+chaosdinosaur@users.noreply.github.com>
2024-04-03 19:39:03 +0000

    .

--- aio/provisioning/00-install-prerequistes.sh
+++ aio/provisioning/00-install-prerequistes.sh
@@ -24,7 +24,6 @@ fi
 if ! command -v k3s >/dev/null 2>&1; then
     echo "K3S not found, installing"
     curl -sfL https://get.k3s.io | sh -
-    curl -sfL https://get.k3s.io | K3S_URL=https://myserver:6443 K3S_TOKEN=mynodetoken sh -
 
     mkdir ~/.kube
     sudo KUBECONFIG=~/.kube/config:/etc/rancher/k3s/k3s.yaml kubectl config view --flatten > ~/.kube/merged
@@ -52,6 +51,25 @@ if ! command -v az >/dev/null 2>&1; then
     curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 else
     echo "Azure CLI already installed"
+fi
+
+# Install mosquitto client
+if ! command -v mosquitto_pub >/dev/null 2>&1; then
+    echo "Mosquitto not found, installing"
+    sudo apt-get update
+    sudo apt-get install mosquitto-clients -y
+else
+    echo "Mosquitto Client already installed"
+fi
+
+# Install mqttui
+if ! command -v mqttui >/dev/null 2>&1; then
+    echo "mqttui not found, installing"
+    wget https://github.com/EdJoPaTo/mqttui/releases/download/v0.19.0/mqttui-v0.19.0-x86_64-unknown-linux-gnu.deb
+    apt-get install ./mqttui-v0.19.0-x86_64-unknown-linux-gnu.deb
+    rm -rf ./mqttui-v0.19.0-x86_64-unknown-linux-gnu.deb
+else
+    echo "mqttui already installed"
 fi
 
 # Install Azure extensions: "connectedk8s,k8s-extension,azure-iot-ops"
